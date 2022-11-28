ced195634cdacf85a42a3dd502006aec77bf3a80
i-muiri <ianmuiri99@gmail.com>
2022-11-28 15:54:25 +0300

    Code challenge Completed

--- speed-detector.js
+++ speed-detector.js
@@ -9,12 +9,12 @@ if (speed < 70)
     {
         console.log ('OK');
     }
-    else if ((speed - 70) % 5 == 0 && (speed - 70) / 5 <= 12)
+    else if (points <= 12)
     {
         
         console.log(`Points: `+ points)
     }
-    else if ( (speed - 70) / 5 > 12 )
+    else if ( points > 12 )
     {
         console.log ('License suspended')
     }
